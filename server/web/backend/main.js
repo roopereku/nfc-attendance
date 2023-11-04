d8a0db00963bf1dc651ad1798a4a4642d7f9ebf7
roopereku <roopereku@gmail.com>
2023-11-04 12:59:09 +0200

    Initial work on frontend

--- server/web/backend/main.js
+++ server/web/backend/main.js
@@ -1,11 +1,31 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = 3000
 
-app.use(express.static('/frontend'))
+let courses = {
+	"ABCD1" : {
+		name: "Test course 1"
+	},
 
-app.get('/', (req, res) => {
-	res.sendFile('/frontend/html/index.html')
+	"ABCD2" : {
+		name: "Test course 2"
+	}
+}
+
+app.use(express.static("/frontend"))
+app.use(express.static("/backend/node_modules/bootstrap/dist/"))
+
+app.get("/", (req, res) => {
+	res.sendFile("/frontend/html/index.html")
+})
+
+app.get("/view/:courseId", (req, res) => {
+	// TODO: Send ID as well.
+	res.sendFile("/frontend/html/view.html")
+})
+
+app.get("/courses", (req, res) => {
+	res.send(JSON.stringify(courses))
 })
 
 app.listen(port, () => {
---
+++ server/web/frontend/js/index.js
@@ -0,0 +1,47 @@
+let courses = {}
+
+function startViewing(e)
+{
+	// Redirect the user to the viewing page.
+	const start = document.getElementById("start-button")
+	location.href = "/view/" + start.dataset.selectedId
+}
+
+function updateSelectedCourse(id)
+{
+	// Update the start button text and store the ID.
+	const start = document.getElementById("start-button")
+	start.dataset.selectedId = id
+	start.innerHTML = "Start viewing " + courses[id].name
+}
+
+function addCoursesToList()
+{
+	const list = document.getElementById("courses-list")
+
+	for(const [key, value] of Object.entries(courses))
+	{
+		let item = document.createElement("li")
+		let element = document.createElement("a")
+
+		element.className = "dropdown-item"
+		element.innerHTML = value.name
+		element.href = "#"
+
+		element.dataset.courseId = key
+
+		item.appendChild(element)
+		list.appendChild(item)
+	}
+}
+
+fetch("/courses")
+	.then((data) => data.json())
+	.then((data) =>
+{
+	courses = data
+	const first = Object.keys(courses)[0]
+
+	addCoursesToList()
+	updateSelectedCourse(first)
+})
