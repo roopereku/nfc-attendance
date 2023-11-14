8f395488e5d07bdbf4fa9d6fbba2ca1b193f3e96
roopereku <roopereku@gmail.com>
2023-11-14 22:17:47 +0200

    Initial work on viewing page

---
+++ server/web/frontend/js/view.js
@@ -0,0 +1,14 @@
+function displayUser(userId, userName)
+{
+	const element = document.createElement("button")
+	const area = document.getElementById("userArea")
+
+	element.className = "btn btn-secondary mx-1 my-1"
+	element.innerHTML = userName
+	element.dataset.id = userId
+
+	area.appendChild(element)
+}
+
+for(let i = 0; i < 20; i++)
+	displayUser("testid", "Test user")
