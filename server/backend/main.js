fac1d865e468f44176e7b671600bf8020e7ef7ed
roopereku <roopereku@gmail.com>
2023-11-21 23:53:17 +0200

    Better conformation to the docs

--- server/backend/main.js
+++ server/backend/main.js
@@ -33,6 +33,43 @@ function validateCourse(id, res)
 	return false
 }
 
+function validateEndpointIdFormat(id, res)
+{
+	// TODO: Ensure that the supplied ID is something that the backend
+	// would provide. For example make sure that the length is 16.
+
+	return true
+}
+
+function validateEndpoint(req, res)
+{
+	// Make sure that the endpoint sent an ID.
+	if("endpointId" in req.body)
+	{
+		// Ensure that the given ID is valid.
+		if(validateEndpointIdFormat(req.body.endpointId, res))
+		{
+			// TODO: Ask the database if the given endpoint exists.
+			// If no, respond with "401 Unauthorized".
+
+			// TODO: Ask the database if the endpoint is blocked.
+			// If yes, respond with "401 Unauthorized".
+
+			return true
+		}
+
+		res.status(400)
+		res.send("Invalid ID format")
+
+		return false
+	}
+
+	res.status(400)
+	res.send("Missing ID")
+
+	return false
+}
+
 const app = express()
 
 app.use(express.static("/frontend"))
@@ -54,22 +91,51 @@ app.get("/dashboard", (req, res) => {
 })
 
 app.post("/endpoint/register", (req, res) => {
+	// Check if the endpoint supplied an existing ID.
+	if("endpointId" in req.body)
+	{
+		// If an ID exists, make sure that endpoint is valid.
+		if(validateEndpoint(req, res))
+		{
+			// TODO: Ask the database if the endpoint is already authorized.
+			// If yes, respond with "200 OK" and send a list of available courses.
+
+			// If the endpoint supplied an existing ID which isn' yet authorized or
+			// blocked, respond with "201 Created" to indicate that the
+			// endpoint is still waiting for authorization.
+			res.status(201)
+			res.send(JSON.stringify({
+				endpointId: req.body.endpointId
+			}))
+		}
+	}
+
 	// TODO: Add the new endpoint into the database.
 	const id = crypto.randomBytes(16).toString("hex")
 
-	res.status(200)
+	// Respond with "201 Created" to indicate that the
+	// endpoint is still waiting for authorization.
+	res.status(201)
 	res.send(JSON.stringify({
 		endpointId: id
 	}))
 })
 
+app.post("/endpoint/join/", (req, res) => {
+	if(validateEndpoint(req, res))
+	{
+	}
+})
+
 app.post("/endpoint/setUserState/", (req, res) => {
-	// TODO: Make sure that whoever made the request is a permitted endpoint.
-	console.log(req.body)
-	res.status(200)
-	res.send(JSON.stringify({
-		userName: "Test user"
-	}))
+	if(validateEndpoint(req, res))
+	{
+		console.log(req.body)
+		res.status(200)
+		res.send(JSON.stringify({
+			userName: "Test user"
+		}))
+	}
 })
 
 app.get("/view/:courseId", (req, res) => {
