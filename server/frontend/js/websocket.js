a12bb6a68cf87a2a233e6a5582adfbd0ccfdf6cc
roopereku <roopereku@gmail.com>
2023-12-08 08:02:30 +0000

    Send a ping every 30 seconds

--- server/frontend/js/websocket.js
+++ server/frontend/js/websocket.js
@@ -1,10 +1,21 @@
 const protocol = location.protocol === "https:" ? "wss" : "ws"
 const ws = new WebSocket(protocol + "://" + location.hostname + websocketPath)
 
+let pingInterval = undefined;
+
 console.log("Websocket connecting to", protocol + "://" + location.hostname + websocketPath)
 
 ws.addEventListener("open", (e) => {
+	// Every 30 seconds, send an empty JSON body. This acts as a heartbeat
+	// message because stale websocket connections die after some amount of time.
+	pingInterval = setInterval(() => {
+		ws.send("{}")
+	}, 30000)
 });
+
+ws.addEventListener("close", (e) => {
+	clearInterval(pingInterval)
+})
 
 ws.addEventListener("message", (e) => {
 	console.log("Message from server ", e.data)
